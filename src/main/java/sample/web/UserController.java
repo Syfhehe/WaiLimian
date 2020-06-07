8e2d4ba41f7e699f212ed02db3e1de2835bb9982
Syfhehe <shenyf811@gmail.com>
2020-06-07 17:52:27 +0800

    Update

--- src/main/java/sample/web/UserController.java
+++ src/main/java/sample/web/UserController.java
@@ -2,6 +2,7 @@ package sample.web;
 
 import io.swagger.annotations.ApiOperation;
 import sample.model.User;
+import sample.repository.UserRepository;
 import sample.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +17,17 @@ public class UserController
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
 
+    // code data 数据都在data里
     @ApiOperation(value="获取用户信息", notes="根据id获取用户信息")
     @GetMapping(value = "/user_info")
-    public Object getUser() throws NotFoundException
+    public Object getUser() throws NotFoundException 
     {
-        User user =  userService.getCurrentUser();
-        user.setPassword(null);
-        return user;
+//        User user =  userService.getCurrentUser();
+//        user.setPassword(null);
+        return userRepository.findOne(1l);
     }
 
 }
